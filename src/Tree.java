cbe0298763d4e1f0b658f464d9490b677a45661e
udaymondal <udaysk.mondal@gmail.com>
2021-01-10 15:08:22 +0600

    Tree added

---
+++ src/Tree.java
@@ -0,0 +1,12 @@
+public class Tree{
+    public static void main(String[] args){
+        //plant tree: lagau
+        System.out.println("Hey there, I’m JavaC and I’m learning to code in Java!");
+        System.out.println("Lagai");
+        System.out.println("  *  ");
+        System.out.println(" *** ");
+        System.out.println(" *** ");
+        System.out.println("  *  ");
+        System.out.println("  *  ");
+    }
+}
